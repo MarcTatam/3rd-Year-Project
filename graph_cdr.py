3cf004ef77320744a8dc2bf978abb41d931d99ef
Marc Tatam <marc.smsjb.tatam@gmail.com>
2021-12-30 14:14:24 +0000

    Heatmap code

--- graph_cdr.py
+++ graph_cdr.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
 import plotly.express as ex
+from datetime import datetime, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -15,30 +16,10 @@ def graph_day_one():
     plt.show()
 
 def heatmap():
-    df = cdr.merge_countries("2013-11-01")
-    with open("milano-grid.geojson") as f:
-        gj = geojson.load(f)
-    points = []
-    i = 0
-    colours = []
-    for feature in gj['features']:
-        if feature['geometry']['type'] == 'Polygon':
-            points.extend(feature['geometry']['coordinates'][0][::-1])    
-            points.append([None, None]) # mark the end of a polygon
-            if i % 2 == 0:
-                colours.append("red")
-            else:
-                colours.append("blue")
-    lons, lats = zip(*points)
-    fig = go.Figure(go.Scattermapbox(mode = "lines", fill = "toself",lon =lons,lat =lats, fillcolor =colours))
-    fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    fig.show()
-
-def heatmap2():
-    df = cdr.merge_countries("2013-11-01")
-    df = df[df[1] == 1383314400000]
+    df = cdr.merge_countries("2013-12-11")
+    df = df[df[1] == 1386788400000]
     df.columns =['CellID', 'Timestamp', 'SMSIN','SMSOUT','CALLIN','CALLOUT','INTERNET']
+    df['CellID'] = df['CellID'] -1
     df['SMSIN'] = np.log2(df['SMSIN'])
     print(df[df['CellID']==5161])
     with open("milano-grid.geojson") as f:
@@ -52,10 +33,46 @@ def heatmap2():
 
                             marker=dict(opacity=0.5)
                            ))
-    fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
+    fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=9.19, mapbox_center_lat=45.4642,mapbox_zoom=10)
+    fig.update_geos(fitbounds="locations")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_traces(colorbar_title_text="Test")
     fig.show()
+    #fig.write_image("./images/fig1.png")
 
+def develop_image(timestamp:int):
+    dt = datetime.fromtimestamp(int(timestamp/1000)) + timedelta(hours = 1)
+    df = cdr.merge_countries(dt.strftime('%Y-%m-%d'))
+    df = df[df[1] == timestamp]
+    df.columns =['CellID', 'Timestamp', 'SMSIN','SMSOUT','CALLIN','CALLOUT','INTERNET']
+    df['CellID'] = df['CellID'] -1
+    df['SMSIN'] = np.log10(df['SMSIN'])
+    with open("milano-grid.geojson") as f:
+        gj = geojson.load(f)
+    fig= go.Figure(go.Choroplethmapbox(
+                            geojson=gj,
+                            locations=df.CellID,
+                            z=df.SMSIN,
+                            zmin = -5,
+                            zmax = 3.4,
+                            colorscale="jet",  
+                            marker_line_width=0,
+                            marker=dict(opacity=0.5)
+                           ))
+    fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=9.19, mapbox_center_lat=45.4642,mapbox_zoom=10)
+    fig.update_geos(fitbounds="locations")
+    fig.update_layout(title_text="Heatmap of SMS Activity " + dt.strftime('%Y-%m-%d %H:%M'))
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_traces(colorbar_title_text="Log(Activity)")
+    fig.add_annotation(text = "SMS Activity: "+dt.strftime('%Y-%m-%d %H:%M'), x=0.2, y=-0.021, bgcolor="white")
+    fig.write_image("./images/"+dt.strftime('sms%Y-%m-%d-%H-%M')+".png")
+
+def develop_images(start:int, stop:int):
+    this = start
+    while this <= stop:
+        print(this)
+        develop_image(this)
+        this += 600000
 if __name__ == "__main__":
-    heatmap2()
+    develop_images(1387209600000, 1388616600000)
 
---
+++ video.py
@@ -0,0 +1,84 @@
+from moviepy.editor import *
+from datetime import datetime, timedelta
+import os
+
+def first_render():
+    image1 = ImageClip("./images/sms2013-11-01-00-00.png")
+    image2 = ImageClip("./images/sms2013-11-01-00-10.png")
+    image3 = ImageClip("./images/sms2013-11-01-00-20.png")
+    image1 = image1.set_duration(0.1)
+    image2 = image2.set_duration(0.1)
+    image3 = image3.set_duration(0.1)
+    image1 = image1.set_start(0)
+    image2 = image2.set_start(0.1)
+    image3 = image3.set_start(0.2)
+    final = CompositeVideoClip([image1,image2,image3])
+    final = final.set_duration(0.3)
+    final.write_videofile("heatmap.mp4",fps=10)
+
+def render(start:int, stop:int):
+    this = start
+    while this <= stop:
+        print(this)
+        dt = datetime.fromtimestamp(int(this/1000))
+        clip = VideoFileClip("heatmap.mp4")
+        dur = clip.duration
+        imageclip = ImageClip("./images/sms"+dt.strftime('%Y-%m-%d-%H-%M')+".png")
+        imageclip = imageclip.set_duration(0.1)
+        imageclip = imageclip.set_start(dur)
+        print(imageclip.start)
+        final = CompositeVideoClip([clip,imageclip])
+        final = final.set_duration(dur+0.1)
+        final.write_videofile("temp.mp4",fps=10)
+        os.remove("heatmap.mp4")
+        os.rename("temp.mp4", "heatmap.mp4")
+        this += 600000
+
+def render_day(date:str):
+    start = 0
+    images = []
+    for hour in range(24):
+        for minute in range(6):
+            imageclip = ImageClip("./images/sms"+date+"-"+str(hour).zfill(2)+"-"+str(minute*10).zfill(2)+".png")
+            imageclip = imageclip.set_duration(0.1)
+            imageclip = imageclip.set_start(start)
+            start += 0.1
+            print(start)
+            images.append(imageclip)
+    final = CompositeVideoClip(images)
+    final.set_duration(14.4)
+    final.write_videofile("./videos/"+date+".mp4",fps=10)
+
+def render_all():
+    for day in range(1,31):
+        print(day)
+        render_day("2013-11-"+str(day).zfill(2))
+    for day in range(1,32):
+        print(day)
+        render_day("2013-12-"+str(day).zfill(2))
+
+
+def join_all():
+    clips = []
+    start = 0
+    for day in range(1,31):
+        print(day)
+        clip = VideoFileClip("./videos/2013-11-"+str(day).zfill(2)+".mp4")
+        clip = clip.set_start(start)
+        start+=14.4
+        clips.append(clip)
+    for day in range(1,32):
+        print(day)
+        clip = VideoFileClip("./videos/2013-12-"+str(day).zfill(2)+".mp4")
+        clip = clip.set_start(start)
+        start+=14.4
+        clips.append(clip)
+    clip = VideoFileClip("./videos/2014-01-01.mp4")
+    clip = clip.set_start(start)
+    clips.append(clip)
+    final = CompositeVideoClip(clips)
+    final.write_videofile("heatmap.mp4", fps = 10)
+
+
+if __name__ == "__main__":
+    join_all()
