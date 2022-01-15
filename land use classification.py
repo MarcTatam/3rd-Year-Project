cb963e3ff2abb4b8a7262ba48bdcfcee5315c552
MarcTatam <marc.smsjb.tatam@gmail.com>
2022-01-15 11:45:35 +0000

    Completed K means

--- land use classification.py
+++ land use classification.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from random import random
 import pandas as pd
 import datetime as dt
 import open_cdr as ocdr
@@ -6,23 +7,108 @@ import json
 import math
 
 class Cell:
-    """representing a cell"""
+    """Class representing a cell"""
     def __init__(self, id:int):
+        """Constructor
+        
+        Args
+        self - instance identifier
+        id - Cell id"""
         self.weekend = []
         self.weekday = []
         self.id = id
 
-    def to_json(self):
+    def to_json(self)->dict:
+        """Converts the cell to a JSON serializable object
+        
+        Args
+        self - instance identifier
+        
+        Returns
+        Object which can be serialized into a json file"""
         return {"weekend" : self.weekend, "weekday" : self.weekday, "id" : self.id}
 
     def distance_to(self, centroid)->float:
+        """Calculates the distance to a centroid
+        
+        Args
+        self - instance identifier
+        centroid - the centroid to calculate the distance to
+        
+        Returns
+        Float for the distance to a value"""
         temp_sum = 0
-        for i in range(23):
+        for i in range(24):
             temp_sum = (self.weekend[i] - centroid.weekend[i])**2
             temp_sum = (self.weekday[i] - centroid.weekday[i])**2
         return math.sqrt(temp_sum)
 
 class Centroid:
+    """Class representing a centroid"""
+    def __init__(self):
+        """Constructor"""
+        self.weekend = [0]*24
+        self.weekday = [0]*24
+        self.cells = []
+
+    def add_cell(self, cell):
+        """Adds a cell to the list of those attached to the centroid
+        
+        Args
+        self - instance identifier
+        cell - cell to attach to the centroid"""
+        self.cells.append(cell)
+
+def attach_to_centroids(cells:[Cell],centroids:[Centroid])->[Centroid]:
+    #Clear centroids of attachments
+    for centroid in centroids:
+        centroid.cells = []
+    for cell in cells:
+        nearest = None
+        distance = None
+        for i in range(len(centroids)):
+            distance_to = cell.distance_to(centroids[i])
+            if distance == None:
+                nearest = i
+                distance = distance_to
+            elif distance_to < distance:
+                nearest = i
+                distance = distance_to
+            centroids[nearest].add_cell(cell)
+    return centroids
+
+def position_centroids(centroids:[Centroid])->[Centroid]:
+    for i in range(len(centroids)):
+        #First sum the positions of each cell
+        sum_weekday = [0]*24
+        sum_weekend = [0]*24
+        for cell in centroids[i].cells:
+            for j in range(24):
+                sum_weekday[j] += cell.weekday[j]
+                sum_weekend[j] += cell.weekend[j]
+        #Then find average position
+        if len(centroids[i].cells) > 0:
+            for j in range(24):
+                sum_weekend[j] = sum_weekend[j]/len(centroids[i].cells)
+                sum_weekday[j] = sum_weekday[j]/len(centroids[i].cells)
+            centroids[i].weekend = sum_weekend
+            centroids[i].weekday = sum_weekday
+    return centroids
+
+def k_means(iterations : int, centroids: int, cells: [Cell]):
+    centroid_list = []
+    for i in range(centroids):
+        this_centroid = Centroid()
+        for j in range(24):
+            this_centroid.weekday[j] = random()
+            this_centroid.weekend[j] = random()
+        centroid_list.append(this_centroid)
+    for i in range(iterations):
+        print(i)
+        print(centroid_list)
+        centroid_list = attach_to_centroids(cells, centroid_list)
+        centroid_list = position_centroids(centroid_list)
+    return centroid_list
 
 
 def save_cells(cell_list: [Cell]):
@@ -41,7 +127,7 @@ def load_cells()->[Cell]:
         cell.weekday = item["weekday"]
         cell.weekend = item["weekend"]
         cells.append(cell)
-    return cell
+    return cells
 
 def parse_data():
     data_list = []
@@ -88,15 +174,20 @@ def format_data(weekday:pd.DataFrame, weekend: pd.DataFrame)->[Cell]:
     return cells
 
 if __name__ == "__main__":
-    cell_list = [Cell(1)]
-    cdr = open_data()
-    cdr = remove_excess(cdr)
-    cdr = sort_daytype(cdr)
-    cdr = cdr.groupby(["0","8","9"]).sum()
-    cdr = cdr.reset_index()
-    weekday = cdr.loc[cdr['8'] == 0]
-    weekend = cdr.loc[cdr['8'] == 1]
-    weekday = normalise(weekday, "7")
-    weekend = normalise(weekend, "7")
-    cells = format_data(weekday, weekend)
-    save_cells(cells)
+    #cell_list = [Cell(1)]
+    #cdr = open_data()
+    #cdr = remove_excess(cdr)
+    #cdr = sort_daytype(cdr)
+    #cdr = cdr.groupby(["0","8","9"]).sum()
+    #cdr = cdr.reset_index()
+    #weekday = cdr.loc[cdr['8'] == 0]
+    #weekend = cdr.loc[cdr['8'] == 1]
+    #weekday = normalise(weekday, "7")
+    #weekend = normalise(weekend, "7")
+    #cells = format_data(weekday, weekend)
+    #save_cells(cells)
+    cells = load_cells()
+    centroids = k_means(5,5, cells)
+    for centroid in centroids:
+        print(len(centroid.cells))
+
