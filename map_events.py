d0c1b98457f81053a5c6a5eca5f22006f4970292
MarcTatam <marc.smsjb.tatam@gmail.com>
2022-02-07 14:18:09 +0000

    Word searching

--- map_events.py
+++ map_events.py
@@ -1,5 +1,149 @@
 import keplergl
+import pandas as pd
+import geojson as gj
 
 def map_events():
     this_map = keplergl.KeplerGl()
+    venues = pd.read_csv("Venues.csv")
+    events = pd.read_csv("Events.csv")
+    df = pd.merge(venues, events, on=["Venue"])
+    df[['Long','Lat']] = df.Coords.str.split(", ",expand=True,)
+    count = {}
+    events_json = {
+        "type": "FeatureCollection",
+        "features": []
+        }
+    for index, Row in df.iterrows():
+        if Row["Venue"] in count.keys():
+            elevation = 50*count[Row["Venue"]]
+            count[Row["Venue"]] += 1
+        else:
+            elevation = 0
+            count[Row["Venue"]] = 1
+        point = {}
+        point["type"] = "Feature"
+        point["geometry"] = {
+            "type": "Point",
+            "coordinates": [float(Row["Lat"]), float(Row["Long"]),elevation]}
+        point["properties"] = {"Venue": Row["Venue"], "Event" : Row["Name"], "Date" : Row["Date"]}
+        events_json["features"].append(point)
+    this_map.add_data(events_json, "Events")
+    this_map.config = {'version': 'v1',
+ 'config': {'visState': {'filters': [],
+   'layers': [{'id': '6695y74',
+     'type': 'geojson',
+     'config': {'dataId': 'Events',
+      'label': 'Events',
+      'color': [76, 154, 78],
+      'highlightColor': [252, 242, 26, 255],
+      'columns': {'geojson': '_geojson'},
+      'isVisible': True,
+      'visConfig': {'opacity': 0.8,
+       'strokeOpacity': 0.8,
+       'thickness': 0.5,
+       'strokeColor': None,
+       'colorRange': {'name': 'Global Warming',
+        'type': 'sequential',
+        'category': 'Uber',
+        'colors': ['#5A1846',
+         '#900C3F',
+         '#C70039',
+         '#E3611C',
+         '#F1920E',
+         '#FFC300']},
+       'strokeColorRange': {'name': 'Global Warming',
+        'type': 'sequential',
+        'category': 'Uber',
+        'colors': ['#5A1846',
+         '#900C3F',
+         '#C70039',
+         '#E3611C',
+         '#F1920E',
+         '#FFC300']},
+       'radius': 10,
+       'sizeRange': [0, 10],
+       'radiusRange': [0, 50],
+       'heightRange': [0, 500],
+       'elevationScale': 5,
+       'enableElevationZoomFactor': True,
+       'stroked': False,
+       'filled': True,
+       'enable3d': False,
+       'wireframe': False},
+      'hidden': False,
+      'textLabel': [{'field': None,
+        'color': [255, 255, 255],
+        'size': 18,
+        'offset': [0, 0],
+        'anchor': 'start',
+        'alignment': 'center'}]},
+     'visualChannels': {'colorField': None,
+      'colorScale': 'quantile',
+      'strokeColorField': None,
+      'strokeColorScale': 'quantile',
+      'sizeField': None,
+      'sizeScale': 'linear',
+      'heightField': None,
+      'heightScale': 'linear',
+      'radiusField': None,
+      'radiusScale': 'linear'}}],
+   'interactionConfig': {'tooltip': {'fieldsToShow': {'Events': [{'name': 'Venue',
+        'format': None},
+       {'name': 'Event', 'format': None},
+       {'name': 'Date', 'format': None}]},
+     'compareMode': False,
+     'compareType': 'absolute',
+     'enabled': True},
+    'brush': {'size': 0.5, 'enabled': False},
+    'geocoder': {'enabled': False},
+    'coordinate': {'enabled': False}},
+   'layerBlending': 'normal',
+   'splitMaps': [],
+   'animationConfig': {'currentTime': None, 'speed': 1}},
+  'mapState': {'bearing': 0,
+   'dragRotate': False,
+   'latitude': 45.455934682435,
+   'longitude': 9.23461965432608,
+   'pitch': 0,
+   'zoom': 10.451941083083048,
+   'isSplit': False},
+  'mapStyle': {'styleType': 'dark',
+   'topLayerGroups': {},
+   'visibleLayerGroups': {'label': True,
+    'road': True,
+    'border': False,
+    'building': True,
+    'water': True,
+    'land': True,
+    '3d building': False},
+   'threeDBuildingColor': [9.665468314072013,
+    17.18305478057247,
+    31.1442867897876],
+   'mapStyles': {}}}}
+    this_map.save_to_html(file_name="Events.html")
 
+def map_venues():
+    this_map = keplergl.KeplerGl()
+    venues = pd.read_csv("Venues.csv")
+    venues[['Long','Lat']] = venues.Coords.str.split(", ",expand=True,)
+    venues_json= {
+        "type": "FeatureCollection",
+        "features": []
+        }
+    i = 0
+    for index, Row in venues.iterrows():
+        i += 1
+        point = {}
+        point["type"] = "Feature"
+        point["geometry"] = {
+            "type": "Point",
+            "coordinates": [float(Row["Lat"]), float(Row["Long"])]}
+        point["properties"] = {"name": Row["Name"]}
+        venues_json["features"].append(point)
+    print(venues_json)
+    this_map.add_data(venues_json, "Venues")
+    this_map.save_to_html(file_name="Venues.html")
+
+
+if __name__ == "__main__":
+    map_events()
--- tweet_processing.py
+++ tweet_processing.py
@@ -2,6 +2,7 @@ import nltk
 import datetime
 import json
 import pickle
+import math
 
 class Word:
     """Class for representing a word"""
@@ -40,7 +41,6 @@ def split_tweet(tweet:str)->[str]:
     tweet_words = tweet.split(" ")
     out_words = []
     for word in tweet_words:
-        word = ''.join(e for e in word if e.isalnum()).lower()
         if word[0:4] == "http" or len(word) == 1 or len(word) == 0:
             pass
         else:
@@ -58,44 +58,56 @@ def load_words()->[Word]:
             words = split_tweet(tweet["text"])
             tweet_time =  datetime.datetime.strptime(tweet["created_at"],"%Y-%m-%dT%H:%M:%S.000Z")
             for word in words:
-                if word in unwanted:
+                if "http://" in word:
+                    pass
+                elif word in unwanted:
                     pass
                 elif word in processed:
                     ind = processed.index(word)
                     processed[ind].increment_ind(i-1,int(tweet_time.strftime("%H")))
                 else:
                     formatted = Word(word)
-                    print(formatted.distribution[i-1][int(tweet_time.strftime("%H"))])
                     formatted.increment_ind(i-1,int(tweet_time.strftime("%H")))
                     processed.append(formatted)
     for i in range(1,32):
         print(i)
-        with open(str(i).zfill(2)+"1113tweets.json","r") as f:
+        with open(str(i).zfill(2)+"1213tweets.json","r") as f:
             tweets = json.load(f)["tweets"]
         for tweet in tweets:
             words = split_tweet(tweet["text"])
             tweet_time =  datetime.datetime.strptime(tweet["created_at"],"%Y-%m-%dT%H:%M:%S.000Z")
             for word in words:
-                if word in unwanted:
+                if "http://" in word:
+                    pass
+                elif word in unwanted:
                     pass
                 elif word in processed:
                     ind = processed.index(word)
                     processed[ind].increment_ind(i-1,int(tweet_time.strftime("%H")))
                 else:
                     formatted = Word(word)
-                    print(formatted.distribution[i-1][int(tweet_time.strftime("%H"))])
                     formatted.increment_ind(i-1,int(tweet_time.strftime("%H")))
                     processed.append(formatted)
     return processed
 
-def save_words(words : [Word]):
-    with open("words.pkl", "wb") as f:
+def save_word(word: Word):
+    with open(word.word+".pkl", "wb") as f:
+        pickle.dump(word,f, protocol = pickle.HIGHEST_PROTOCOL)
+
+def save_words(words : [Word], filename : str):
+    with open(filename, "wb") as f:
         pickle.dump(words, f, protocol = pickle.HIGHEST_PROTOCOL)
 
-def open_words()->[Word]:
-    with open("words.pkl", "rb") as f:
+def open_word(word:str)->[Word]:
+    with open(word+".pkl", "rb") as f:
+        word_obj =  pickle.load(f)
+    return word_obj
+
+def open_words(filename)->[Word]:
+    with open(filename, "rb") as f:
         word =  pickle.load(f)
     return word
+
 
 def tweet_count():
     count = 0
@@ -110,11 +122,65 @@ def tweet_count():
         count += len(tweets)
         print(count)
 
+def normalise(words:[Word])->[Word]:
+    z = 0
+    for word in words:
+        z += 1
+        print(z)
+        total = 0
+        for i in range(61):
+            total += sum(word.distribution[i])
+        if total > 1 :
+            average = total/(61*24)
+            sd_sum = 0
+            for i in range(61):
+                for j in range(24):
+                    sd_sum = (word.distribution[i][j] - average)**2
+            sd = math.sqrt(sd_sum/(61*24))
+            for i in range(61):
+                for j in range(24):
+                    word.distribution[i][j] = (word.distribution[i][j] - average)/sd
+    return words
+
+def search_word(word:str)->Word:
+    word_obj = Word(word)
+    for i in range(1,31):
+        with open(str(i).zfill(2)+"1113tweets.json","r") as f:
+            tweets = json.load(f)["tweets"]
+            for tweet in tweets:
+                tweet_time =  datetime.datetime.strptime(tweet["created_at"],"%Y-%m-%dT%H:%M:%S.000Z")
+                content = tweet["text"]
+                word_obj.distribution[i-1][int(tweet_time.strftime("%H"))] += content.lower().count(word)
+    true_base = 30
+    for i in range(1,32):
+        with open(str(i).zfill(2)+"1213tweets.json","r") as f:
+            tweets = json.load(f)["tweets"]
+            for tweet in tweets:
+                tweet_time =  datetime.datetime.strptime(tweet["created_at"],"%Y-%m-%dT%H:%M:%S.000Z")
+                content = tweet["text"]
+                word_obj.distribution[true_base + i-1][int(tweet_time.strftime("%H"))] += content.lower().count(word)
+    save_word(word_obj)
+
+def normalise_word(word: Word):
+    total = 0
+    for i in range(61):
+        total += sum(word.distribution[i])
+    if total > 1 :
+        average = total/(61*24)
+        sd_sum = 0
+        for i in range(61):
+            for j in range(24):
+                sd_sum = (word.distribution[i][j] - average)**2
+        sd = math.sqrt(sd_sum/(61*24))
+        for i in range(61):
+            for j in range(24):
+                word.distribution[i][j] = (word.distribution[i][j] - average)/sd
+    return word
 
 if __name__ == "__main__":
     #nltk.download("stopwords")
-    words = load_words()
-    save_words(words)
-    #tweet_count()
+    #search_word("stadio")
+    print(normalise_word(open_word("ac milan")))
+
 
 
