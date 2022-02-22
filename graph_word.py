8f4ce11f343727b8a90f686759a24e817b5566cb
MarcTatam <marc.smsjb.tatam@gmail.com>
2022-02-22 12:58:54 +0000

    Z-Score normilisation on word usage

--- graph_word.py
+++ graph_word.py
@@ -1,4 +1,4 @@
-from tweet_processing import Word, open_word, normalise_word
+from tweet_processing import Word, open_word, normalise_word, WordDay, open_word_day, containing_tweets, similar_words, residual_base, residual_pattern, normalise_word_day, search_word_day
 import matplotlib.pyplot as plt
 import project_utils as util
 import pandas as pd
@@ -64,5 +64,97 @@ def graph_word_me(word:str, events:[str]):
     ax.set_title(word)
     plt.savefig(word+'.png')
 
+def graph_word_day_me(word:str, events:[str]):
+    """Graphs the usage of a word, highlighting the relevant events
+    
+    Args
+    word - word to plot the usage of
+    events - events to highlight"""
+    df = pd.read_csv("Events.csv")
+    fig, ax = plt.subplots()
+    word_obj = open_word_day(word)
+    for event in events:
+        for index, row in df.loc[df["Name"] == event].iterrows():
+            day = util.convert_to_project(row["Date"])
+            ax.axvline(day, color = "red", linestyle = "--")
+    y = word_obj.distribution
+    x = []
+    for i in range(61):
+        x.append(i)
+    ax.plot(x,y)
+    ax.set_ylabel("Word Usage")
+    ax.set_xlabel("Days Since Project Epoch")
+    ax.set_title(word)
+    plt.savefig(word+'day.png')
+
+def plot_related_frequency(word :str):
+    tweets = containing_tweets(word)
+    similar_dict = similar_words(tweets, word)
+    dict2 = {}
+    for key in similar_dict.keys():
+        if similar_dict[key] > 5:
+            dict2[key] = similar_dict[key]
+    x = list(dict2.keys())
+    y = []
+    for key in x:
+        y.append(dict2[key])
+    fig, ax = plt.subplots()
+    ax.bar(x,y)
+    plt.xticks(fontsize=8, rotation = 90)
+    fig.tight_layout()
+    plt.savefig(word+'freq.png')
+
+def plot_related_time(word:str, events: [str]):
+    df = pd.read_csv("Events.csv")
+    fig, ax = plt.subplots()
+    base = residual_base()
+    tweets = containing_tweets(word)
+    word_obj = search_word_day(word)
+    normalised_obj = normalise_word_day(word_obj)
+    residual = residual_pattern(base, normalised_obj)
+    x = []
+    for i in range(61):
+        x.append(i)
+    ax.plot(x, residual.distribution)
+    for event in events:
+        for index, row in df.loc[df["Name"] == event].iterrows():
+            day = util.convert_to_project(row["Date"])
+            ax.axvline(day, color = "red", linestyle = "--")
+    ax.axhline(0, color = "black", linestyle = "--")
+    similar_dict = similar_words(tweets, word)
+    dict2 = {}
+    lines = []
+    labels = []
+    for key in similar_dict.keys():
+        if similar_dict[key] > 5:
+            word_obj = search_word_day(key)
+            normalised_obj = normalise_word_day(word_obj)
+            residual = residual_pattern(base, normalised_obj)
+            line, = ax.plot(x, residual.distribution)
+            lines.append(line)
+            labels.append(key)
+    ax.set_ylabel("Residual Error from Z-Score")
+    ax.set_xlabel("Time from Project Epoch")
+    ax.set_title("Z-Score Residual Difference of words relating to " + word)
+    plt.legend(lines,labels,prop={'size': 5}, loc="upper right")
+    plt.savefig(word+'related.png')
+
+def graph_alcatraz():
+    data = pd.read_csv("AlcatrazMilano.csv",parse_dates=True)
+    df = pd.read_csv("Events.csv")
+    ax = data.plot()
+    for index, row in df.loc[df["Venue"] == "Alcatraz"].iterrows():
+        day = util.convert_to_project(row["Date"])
+        ax.axvline(day, color = "red", linestyle = "--")
+    ax.set_title("Number of Tweets from @AlcatrazMilano")
+    ax.set_ylabel("Number of Tweets")
+    ax.set_xlabel("Hours Since Project Epoch")
+    plt.savefig("AlcatrazTweets.png")
+
 if __name__ == "__main__":
-    graph_word_me("skrillex", ["Skrillex"])
+    graph_word_day_me("internazionale", ["Inter vs Livorno", "Inter vs Sampdoria", "Inter vs Parma", "Inter vs Milan"])
+    #graph_word_day_me("vs", ["Inter vs Livorno", "Inter vs Sampdoria", "Inter vs Parma", "Inter vs Milan","Milan vs Genoa","Milan vs Fiorentina","Milan vs Roma"])
+    #graph_word_day_me("arctic monkeys", ["Arctic Monkeys"])
+    #plot_related_time("internazionale",["Inter vs Livorno", "Inter vs Sampdoria", "Inter vs Parma", "Inter vs Milan"])
+    #plot_related_frequency("internazionale")
+    #graph_alcatraz()
--- tweet_processing.py
+++ tweet_processing.py
@@ -3,6 +3,7 @@ import datetime
 import json
 import pickle
 import math
+import numpy as np
 
 class Word:
     """Class for representing a word"""
@@ -29,6 +30,11 @@ class Word:
         for i in range(61):
             count += sum(self.distribution[i])
         return self.word + " : " + str(count)
+
+class WordDay(Word):
+    def __init__(self, word:str):
+        self.word = word
+        self.distribution = [0]*61
 
 def split_tweet(tweet:str)->[str]:
     """Splits a tweet into the words it contains and filters out links
@@ -90,6 +96,26 @@ def load_words()->[Word]:
                     processed.append(formatted)
     return processed
 
+def containing_tweets(word:str)->[dict]:
+    out = []
+    for i in range(1,31):
+        with open(str(i).zfill(2)+"1113tweets.json","r") as f:
+            tweets = json.load(f)["tweets"]
+            for tweet in tweets:
+                content = tweet["text"]
+                if content.lower().count(word) > 0:
+                    out.append(tweet)
+    true_base = 29
+    for i in range(1,32):
+        with open(str(i).zfill(2)+"1213tweets.json","r") as f:
+            tweets = json.load(f)["tweets"]
+            for tweet in tweets:
+                content = tweet["text"]
+                if content.count(word) > 0:
+                    out.append(tweet)
+    return out
+                
+
 def save_word(word: Word):
     with open(word.word+".pkl", "wb") as f:
         pickle.dump(word,f, protocol = pickle.HIGHEST_PROTOCOL)
@@ -98,8 +124,17 @@ def save_words(words : [Word], filename : str):
     with open(filename, "wb") as f:
         pickle.dump(words, f, protocol = pickle.HIGHEST_PROTOCOL)
 
+def save_word_day(word: WordDay):
+    with open(word.word+"day.pkl", "wb") as f:
+        pickle.dump(word,f, protocol = pickle.HIGHEST_PROTOCOL)
+
 def open_word(word:str)->[Word]:
     with open(word+".pkl", "rb") as f:
+        word_obj =  pickle.load(f)
+    return word_obj
+
+def open_word_day(word:str)->[WordDay]:
+    with open(word+"day.pkl", "rb") as f:
         word_obj =  pickle.load(f)
     return word_obj
 
@@ -151,15 +186,34 @@ def search_word(word:str)->Word:
                 tweet_time =  datetime.datetime.strptime(tweet["created_at"],"%Y-%m-%dT%H:%M:%S.000Z")
                 content = tweet["text"]
                 word_obj.distribution[i-1][int(tweet_time.strftime("%H"))] += content.lower().count(word)
-    true_base = 30
+    true_base = 29
     for i in range(1,32):
         with open(str(i).zfill(2)+"1213tweets.json","r") as f:
             tweets = json.load(f)["tweets"]
             for tweet in tweets:
                 tweet_time =  datetime.datetime.strptime(tweet["created_at"],"%Y-%m-%dT%H:%M:%S.000Z")
                 content = tweet["text"]
-                word_obj.distribution[true_base + i-1][int(tweet_time.strftime("%H"))] += content.lower().count(word)
+                word_obj.distribution[true_base + i][int(tweet_time.strftime("%H"))] += content.lower().count(word)
     save_word(word_obj)
+
+def search_word_day(word:str)->Word:
+    word_obj = WordDay(word)
+    for i in range(1,31):
+        with open(str(i).zfill(2)+"1113tweets.json","r") as f:
+            tweets = json.load(f)["tweets"]
+            for tweet in tweets:
+                tweet_time =  datetime.datetime.strptime(tweet["created_at"],"%Y-%m-%dT%H:%M:%S.000Z")
+                content = tweet["text"]
+                word_obj.distribution[i-1] += content.lower().count(word)
+    true_base = 29
+    for i in range(1,32):
+        with open(str(i).zfill(2)+"1213tweets.json","r") as f:
+            tweets = json.load(f)["tweets"]
+            for tweet in tweets:
+                tweet_time =  datetime.datetime.strptime(tweet["created_at"],"%Y-%m-%dT%H:%M:%S.000Z")
+                content = tweet["text"]
+                word_obj.distribution[true_base+i] += content.lower().count(word)
+    return word_obj
 
 def normalise_word(word: Word):
     total = 0
@@ -177,10 +231,104 @@ def normalise_word(word: Word):
                 word.distribution[i][j] = (word.distribution[i][j] - average)/sd
     return word
 
+def normalise_word_day(word: WordDay)->WordDay:
+    mean = np.mean(word.distribution)
+    sd = np.std(word.distribution)
+    for i in range(61):
+        word.distribution[i] = (word.distribution[i]-mean)/sd
+    return word
+
+def residual_base():
+    out = []
+    for i in range(1,31):
+        with open(str(i).zfill(2)+"1113tweets.json","r") as f:
+            tweets = json.load(f)["tweets"]
+            out.append(len(tweets))
+    for i in range(1,32):
+        with open(str(i).zfill(2)+"1213tweets.json","r") as f:
+            tweets = json.load(f)["tweets"]
+            out.append(len(tweets))
+    mean = np.mean(out)
+    sd = np.std(out)
+    for i in range(61):
+        out[i] = (out[i]-mean)/sd
+    return out
+
+def residual_pattern(base: [int], word:WordDay)->WordDay:
+    for i in range(61):
+        word.distribution[i] -= base[i]
+    return word
+        
+def similar_words(tweets:[dict], input_word:str)->dict:
+    unwanted = nltk.corpus.stopwords.words("italian")
+    out = {}
+    for tweet in tweets:
+        words = split_tweet(tweet["text"])
+        for word in words:
+            if word not in unwanted and word.lower().count(input_word) == 0:
+                if word in out.keys():
+                    out[word] += 1
+                else:
+                    out[word] = 1
+    return out
+
+def extra_tweets(start_word:str)->int:
+    unwanted = nltk.corpus.stopwords.words("italian")+nltk.corpus.stopwords.words("english")+["I'm", "w/"]
+    word_dict = {}
+    tweets = containing_tweets(start_word)
+    seen_ids = set()
+    for tweet in tweets:
+        words = split_tweet(tweet["text"])
+        seen_ids.add(tweet["id"])
+        for word in words:
+            if word not in unwanted and word.lower().count(start_word) == 0:
+                if word in word_dict.keys():
+                    word_dict[word] += 1
+                else:
+                    word_dict[word] = 1
+    word_set = set()
+    extra = 0
+    for key in word_dict.keys():
+        if word_dict[key] > 6:
+            word_set.add(key)
+    for word in word_set:
+        print(word+":"+ str(sum(search_word_day(word.lower()).distribution)))
+    print(word_dict)
+    #return extra
+    for i in range(1,31):
+        with open(str(i).zfill(2)+"1113tweets.json","r") as f:
+            tweets = json.load(f)["tweets"]
+            for tweet in tweets:
+                if not tweet["id"] in seen_ids:
+                    for word in word_set:
+                        if tweet["text"].count(word) > 0:
+                            extra += 1
+                            break
+    true_base = 29
+    for i in range(1,32):
+        with open(str(i).zfill(2)+"1213tweets.json","r") as f:
+            tweets = json.load(f)["tweets"]
+            for tweet in tweets:
+                if not tweet["id"] in seen_ids:
+                    for word in word_set:
+                        if tweet["text"].count(word) > 0:
+                            extra += 1
+                            break
+    return extra
+
 if __name__ == "__main__":
     #nltk.download("stopwords")
-    search_word("arctic monkeys")
-    print(normalise_word(open_word("arctic monkeys")))
+    word = search_word_day("internazionale")
+    #print(word.distribution)
+    word = normalise_word_day(word)
+    base_pattern = residual_base()
+    #word = open_word_day("#inter")
+    word = residual_pattern(base_pattern,word)
+    save_word_day(word)
+    #tweets = containing_tweets("internazionale")
+    #print(similar_words(tweets, "internazionale"))
+    #print(extra_tweets("ac milan"))
+
 
 
 
